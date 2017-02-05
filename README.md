9df83444b042423c48bb8d6449b26dce5d1cf9af
Quit Bad Journalism <quitbadjournalism@gmail.com>
2017-02-05 20:47:55 +0100

    some minor updates

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
 # Quit Bad Journalism
 
-Originally based off [Daily Mail Blocker by Jonic](https://github.com/Jonic/DailyMailBlocker), just with support for blocking more terrible media websites!
+A Chrome extension built to block access to websites whose business model relies on bad, trashy or hateful journalism. Nominate more by emailing URLs to [quitbadjournalism@gmail.com](mailto:quitbadjournalism@gmail.com) or by submitting a pull-request.
+
+Have other sites which you don't agree with? You can add custom URLs/RegExp through the extension's options.
+
+Just so you don't get fully locked into some kind of echo chamber, there is a link to allow one to access the blocked page.
+
+
+## Why?
+
+Lots of businesses and websites big and small make their money by selling advertising space. It's important for them to produce and/or publish content that attracts viewers.
+
+Some of these publishing entities exercise a significant lack of journalistic integrity — nee, even a disheartening absence of respect for the general public — and present a form of content as polluting and rotten as '80s [Troma](https://www.youtube.com/watch?v=dMGCPRJNKUg)-grade nuclear waste on society.
+
+Obviously, each person to their own tastes. However, if you are an individual that wants to be a little more proactive in reducing the revenue these poor taste websites make, **Quit Bad Journalism** is a tool to catch and warn against the dangers of clickbait, tabloids, fake news and alternative facts.
+
+
+## Notes
+
+* Originally based off [Daily Mail Blocker by Jonic](https://github.com/Jonic/DailyMailBlocker), just with support for blocking more than just one terrible media website!
+* GIFs provided by [GIPHY API](http://api.giphy.com/)
+
+
+## License
+
+```
+           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+                   Version 2, December 2004
+
+Copyright (C) 2004 Sam Hocevar <sam@hocevar.net>
+
+Everyone is permitted to copy and distribute verbatim or modified
+copies of this license document, and changing it is allowed as long
+as the name is changed.
+
+           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+ 0. You just DO WHAT THE FUCK YOU WANT TO.
+```
--- script.js
+++ script.js
@@ -1,133 +1,152 @@
-function escapeRegExpString (input) {
-  return input.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&")
-}
+/**
+ * Quit Bad Journalism
+ */
+(function (chrome, window, undefined) {
 
-function getAjax (options) {
-  var oReq = new XMLHttpRequest()
-  options = options || {}
-
-  // Load
-  oReq.addEventListener('load', function () {
-    if (options.load && typeof options.load === 'function') {
-      var data = JSON.parse(oReq.responseText)
-      options.load(data, oReq)
-    }
-  })
-
-  // Error
-  oReq.addEventListener('error', function () {
-    if (options.error && typeof options.error === 'function') {
-      options.error(oReq.responseText, oReq)
-    }
-  })
-
-  // Open + send
-  oReq.open(options.method || 'GET', options.url)
-  oReq.send()
-
-  return oReq
-}
-
-var currentLocation = window.location
-
-// User bad sites
-var options = {
-  badSites: [],
-  enableGIFs: true
-}
-chrome.storage.sync.get({
-  badSites: [],
-  enableGIFs: true
-}, function(items) {
-  var getBadSites = []
-
-  // Ensure they're regexps
-  if (items.badSites && items.badSites instanceof Array) {
-    for (var i = 0; i < items.badSites.length; i++) {
-      getBadSites.push(new RegExp(escapeRegExpString(items.badSites[i]), 'i'))
-    }
+  function escapeRegExpString (input) {
+    return input.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&")
   }
 
-  // Set the options
-  options.badSites = getBadSites
-  options.enableGIFs = items.enableGIFs
-})
+  function getAjax (options) {
+    var oReq = new XMLHttpRequest()
+    options = options || {}
 
-// Always blocked!
-var badSites = {
-  breitbart: [
-    /breitbart(\.co|m)?(\.(au|us|uk))?/i
-  ],
-  dailymail: [
-    /dailym\.ai/i,
-    /dailymail(\.co|m)?(\.(au|us|uk|ie))?/i,
-    /mailonsunday(\.co|m)?(\.(au|us|uk))?/i,
-    /mymail\.co\.uk/i
-  ],
-  thesun: [
-    /thesun(\.co|m)?(\.(au|us|uk|ie))?/i,
-    /thescottishsun\.co\.uk/i
-  ],
-  user: badSites
-}
+    // Load
+    oReq.addEventListener('load', function () {
+      if (options.load && typeof options.load === 'function') {
+        var data = JSON.parse(oReq.responseText)
+        options.load(data, oReq)
+      }
+    })
 
-var customBadSites
+    // Error
+    oReq.addEventListener('error', function () {
+      if (options.error && typeof options.error === 'function') {
+        options.error(oReq.responseText, oReq)
+      }
+    })
 
-function blockBadSite (loc, url, img) {
-  document.write('<div style="text-align: center">' +
-    '<h1>Hi there!</h1>' +
-    '<h2 style="font-weight:normal"><pre><code>' + loc + '</code></pre></h2>' +
-    '<h2 style="font-weight:normal">was blocked by <strong>Quit Bad Journalism</strong> &mdash; <em>You\'re welcome!</em></h2>' +
-    '<p><a href="https://github.com/qbj/qbj-chrome" target="_blank">Why?</a></p>' +
-    (img || '') +
-    '<marquee style="margin-top: 100px; margin-bottom: 100px"><a href=\"' + url + '\">Let me in &mdash; I\'m aware of the dangers to my mental health that continuing to this website will expose me to.</a></marquee>' +
-    '</div>')
-}
+    // Open + send
+    oReq.open(options.method || 'GET', options.url)
+    oReq.send()
 
-if (!/_qbj_allow_/i.test(currentLocation)) {
-  var allowThroughURL
-
-  // Already has query vars
-  if (/\?/.test(currentLocation)) {
-    allowThroughURL = currentLocation + '&amp;_qbj_allow_=1'
-  } else {
-    allowThroughURL = currentLocation + '?_qbj_allow_=1'
+    return oReq
   }
 
-  for (var i in badSites) {
-    if (badSites.hasOwnProperty(i)) {
-      for (var j = 0; j < badSites[i].length; j++) {
-        if (badSites[i][j].test(currentLocation)) {
-          // Get a GIF!
-          if (options.enableGIFs) {
-            getAjax({
-              url: 'http://api.giphy.com/v1/gifs/search?q=dance&limit=20&rating=g&api_key=dc6zaTOxFJmzC',
-              load: function (data, xhr) {
-                var img = ''
+  // User bad sites
+  chrome.storage.sync.get({
+    badSites: [],
+    enableGIFs: true
+  }, function (items) {
+    // Default options
+    var options = {
+      badSites: [],
+      enableGIFs: true
+    }
 
-                // Got a random GIF
-                if (data.hasOwnProperty('data')) {
-                  var getRandomGIF = data.data[Math.floor(Math.random() * data.data.length)]
+    // Ensure they're regexps
+    var getBadSites = []
+    if (items.badSites && items.badSites instanceof Array) {
+      for (var i = 0; i < items.badSites.length; i++) {
+        if (/^\//.test(items.badSites[i])) {
+          var re = (items.badSites[i]+'').replace(/^\//, '').replace(/\/.*$/, '')
+          getBadSites.push(new RegExp(re, 'i'))
+        } else {
+          getBadSites.push(new RegExp(escapeRegExpString(items.badSites[i]), 'i'))
+        }
+      }
+    }
 
-                  if (getRandomGIF) {
-                    img = '<a href="' + getRandomGIF.url + '" target="_blank"><img src="' + getRandomGIF.images.original.url + '" alt="" /></a>' +
-                      '<p>GIFs provided by <a href="http://giphy.com" target="_blank">GIPHY</a></p>'
+    // Set the options
+    options.badSites = getBadSites
+    options.enableGIFs = items.enableGIFs
+
+    // Found it!
+    init(options)
+  })
+
+  function blockBadSite (loc, url, img) {
+    document.write('<div style="text-align: center">' +
+      '<h1>Hi there!</h1>' +
+      '<h2 style="font-weight:normal"><pre><code>' + loc + '</code></pre></h2>' +
+      '<h2 style="font-weight:normal">was blocked by <strong>Quit Bad Journalism</strong> &mdash; <em>You\'re welcome!</em></h2>' +
+      '<p><a href="https://github.com/quitbadjournalism/qbj-chrome" target="_blank">Why?</a></p>' +
+      (img || '') +
+      '<marquee style="margin-top: 100px; margin-bottom: 100px"><a href=\"' + url + '\" style="display: block">Let me in &mdash; I\'m aware of the dangers to my mental health that continuing to this website will expose me to.</a></marquee>' +
+      '</div>')
+  }
+
+  function init (options) {
+    var badSites = {
+      breitbart: [
+        /breitbart(\.co|m)?(\.(au|us|uk))?/i
+      ],
+      dailymail: [
+        /dailym\.ai/i,
+        /dailymail(\.co|m)?(\.(au|us|uk|ie))?/i,
+        /mailonsunday(\.co|m)?(\.(au|us|uk))?/i,
+        /mymail\.co\.uk/i
+      ],
+      thesun: [
+        /thesun(\.co|m)?(\.(au|us|uk|ie))?/i,
+        /thescottishsun\.co\.uk/i
+      ],
+      user: options.badSites
+    }
+
+    // Random keywords for getting the
+    var gifKeywords = ['dance', 'woohoo', 'happy', 'sweet+relief', 'relieved', 'thank+god', 'huzzah', 'thank+you', 'mucho+gracias', 'hallelujah', 'you%27re+welcome']
+    var gifKeyword = gifKeywords[Math.floor(Math.random() * gifKeywords.length)]
+
+    // Test if the location will be blocked or allowed through
+    var currentLocation = window.location
+    if (!/_qbj_allow_/i.test(currentLocation)) {
+      var allowThroughURL
+
+      // Already has query vars
+      if (/\?/.test(currentLocation)) {
+        allowThroughURL = currentLocation + '&amp;_qbj_allow_=1'
+      } else {
+        allowThroughURL = currentLocation + '?_qbj_allow_=1'
+      }
+
+      // Check all the bad sites to see if the location matches
+      for (var i in badSites) {
+        if (badSites.hasOwnProperty(i)) {
+          for (var j = 0; j < badSites[i].length; j++) {
+            if (badSites[i][j].test(currentLocation)) {
+              // Get a GIF!
+              if (options.enableGIFs) {
+                getAjax({
+                  url: 'http://api.giphy.com/v1/gifs/search?q=' + gifKeyword + '&limit=20&rating=g&api_key=dc6zaTOxFJmzC',
+                  load: function (data, xhr) {
+                    var img = ''
+
+                    // Got a random GIF
+                    if (data.hasOwnProperty('data')) {
+                      var getRandomGIF = data.data[Math.floor(Math.random() * data.data.length)]
+
+                      if (getRandomGIF) {
+                        img = '<a href="' + getRandomGIF.url + '" target="_blank"><img src="' + getRandomGIF.images.original.url + '" alt="" /></a>' +
+                              '<p>GIFs provided by <a href="http://giphy.com" target="_blank">GIPHY</a></p>'
+                      }
+                    }
+
+                    blockBadSite(currentLocation, allowThroughURL, img)
+                  },
+                  error: function (data, xhr) {
+                    blockBadSite(currentLocation, allowThroughURL)
                   }
-                }
-
-                blockBadSite(currentLocation, allowThroughURL, img)
-              },
-              error: function (data, xhr) {
+                })
+              } else {
                 blockBadSite(currentLocation, allowThroughURL)
               }
-            })
-          } else {
-            blockBadSite(currentLocation, allowThroughURL)
+
+              break
+            }
           }
-          
-          break
         }
       }
     }
   }
-}
+})(chrome, window)
